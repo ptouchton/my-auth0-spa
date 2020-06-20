89065efda771c1a52ed9d6e14c5e63e3779bf2be
Paul Touchton <p_touchton@yahoo.com>
2020-06-20 09:14:32 -0700

    added profile info page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './utilities/profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
